9792bbf8f3ada1a1f42197869eeacc8c7cd590d8
AhmedAbonashy <ahmedabonashy1000@gmail.com>
2023-01-28 06:53:34 -0800

    initia Project added empty home screen added developmen & feature

--- lib/home_screen/HomeScreen.dart
+++ lib/home_screen/HomeScreen.dart
@@ -5,6 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Container(
+      child: Text(
+        '',
+        style: TextStyle(color: Colors.amber, fontSize: 60),
+      ),
+    );
   }
 }
